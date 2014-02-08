23038b163f688c75e91a3225ad5a1523b6634cb9
hizzgdev <hizzgdev@163.com>
2014-02-08 17:09:03 +0800

    developing:add some fragment

--- js/jsmind.js
+++ js/jsmind.js
@@ -7,7 +7,7 @@
  *             data :'http://localhost:8080/example.json'
  *             sync : false,
  *         },
- *         viewer:{
+ *         view:{
  *             container:containerid or dom object,
  *             resize:'auto'
  *         },
@@ -63,10 +63,16 @@
      */
     var DEFAULT_OPTIONS = {
         data:{
-            type : 'url',
-            data : 'http://localhost:8080/example/default_data.json',
+            //type : 'remote',
+            //data : 'http://localhost:8080/example/default_data.json'
+            type : 'local',
+            data : [
+                    {nodeid:'a001', isroot:true, topic:'root node', nodeindex:0},
+                    {nodeid:'b001', parentid:'a001', topic:'sub node #1', summary:'summary of node #1', nodeindex:1 },
+                    {nodeid:'b002', parentid:'a001', topic:'sub node #2', summary:'summary of node #2', nodeindex:2 }
+                   ]
         },
-        viewer:{
+        view:{
             resize:'auto'
         },
         layout:{
@@ -77,9 +83,9 @@
         theme:{
             name:'default'
         },
-        override:{
+        provider:{
             data : null,
-            viewer : null,
+            view : null,
             layout : null,
             theme : null
         }
@@ -94,13 +100,13 @@
          */
         var opts = {};
         for (var o in DEFAULT_OPTIONS) {opts[o] = DEFAULT_OPTIONS[o];}
-        for (var o in options) {for (var k in options[o]) {opts[o][k] = options[o][k];}}
+        for (var o in options) {if(o in opts){for (var k in options[o]){opts[o][k] = options[o][k];}}else{opts[o]=options[o];}}
         this.options = opts;
 
         // function provider
         this.data = null;
         this.layout = null;
-        this.viewer = null;
+        this.view = null;
         this.theme = null;
 
         // init
@@ -110,18 +116,18 @@
     jsMind.prototype={
         init : function(){
             var opts = this.options;
-            var override = opts.override;
+            var provider = opts.provider;
 
-            var data_provider = (!!override.data)? override.data : jsMind._data;
-            var layout_provider = (!!override.layout)? override.layout : jsMind._layout;
-            var viewer_provider = (!!override.viewer)? override.viewer : jsMind._viewer;
-            var theme_provider = (!!override.theme)? override.theme : jsMind._theme;
+            var _data_provider = (!!provider.data)? provider.data : jsMind.data_provider;
+            var _layout_provider = (!!provider.layout)? provider.layout : jsMind.layout_provider;
+            var _view_provider = (!!provider.view)? provider.view : jsMind.view_provider;
+            var _theme_provider = (!!provider.theme)? provider.theme : jsMind.theme_provider;
 
             // create instance of function provider 
-            this.data = new data_provider(this, opts.data);
-            this.layout = new layout_provider(this, opts.layout);
-            this.viewer = new viewer_provider(this, opts.viewer);
-            this.theme = new theme_provider(this, opts.theme);
+            this.data = new _data_provider(this, opts.data);
+            this.layout = new _layout_provider(this, opts.layout);
+            this.view = new _view_provider(this, opts.view);
+            this.theme = new _theme_provider(this, opts.theme);
         },
 
         // show mindmap async
@@ -131,8 +137,8 @@
                 _console.debug('data.load ok');
                 jm.layout.layout(function(){
                     _console.debug('layout.layout ok');
-                    jm.viewer.show(function(){
-                        _console.debug('viewer.show ok');
+                    jm.view.show(function(){
+                        _console.debug('view.show ok');
                     });
                 });
             });
@@ -148,32 +154,124 @@
         return jm;
     };
 
-    jsMind._data = function(jm, options){
+    // ============== static object ==============================================
+
+    jsMind.Node = function(sId,iIndex,sTopic,sSummary,bIsRoot,oParent){
+        this.Id = sId;
+        this.Index = iIndex;
+        this.Topic = sTopic;
+        this.Summary = sSummary;
+        this.IsRoot = bIsRoot;
+        this.Parent = oParent;
+        this.Children = [];
+        this.Data = {};
+    };
+
+    // data provider
+    jsMind.data_provider = function(jm, options){
         this.jsMind = jm;
         this.opts = options;
         this.root = null;
         this.nodes = [];
+        this.load_success_callback = null;
     };
-    jsMind._layout = function(jm, options){this.jsMind = jm; this.opts = options;};
-    jsMind._viewer= function(jm, options){this.jsMind = jm; this.opts = options;};
-    jsMind._theme= function(jm, options){this.jsMind = jm; this.opts = options;};
-
-    jsMind._data.prototype={
+    jsMind.data_provider.prototype={
         load:function(fnCallback){
             _console.debug('data.load');
             if(typeof(fnCallback) === 'function'){
-                fnCallback();
+                this.load_success_callback = fnCallback;
+            }else{
+                this.load_success_callback = null;
+                _console.error('callback is not a function:'+data_type);
+            }
+
+            var data_type = this.opts.type;
+            var data = this.opts.data;
+            if(data_type == 'local'){
+                this.parse(data);
+            }else if(data_type == 'remote'){
+                this.fetch(data);
+            }else{
+                _console.error('unsupported data type :'+data_type);
+            }
+            _console.debug(this);
+        },
+        fetch:function(url){
+            // do ajax ....
+            var node_array = [];
+            this.parse(node_array);
+        },
+        parse:function(node_array){
+            // reverse array for improving looping performance
+            node_array.reverse();
+            var root_node = this._parse_extract_root(node_array);
+            _console.debug(root_node);
+            if(!!root_node){
+                this.root = root_node;
+                this._parse_extract_subnode(node_array, root_node);
+            }else{
+                _console.error('the root node can not be found');
+            }
+            if(!!this.load_success_callback){
+                this.load_success_callback();
             }
         },
-        parse:function(json){},
+        _parse_node:function(node_json, parent_node, isroot){
+            var o = node_json;
+            var p = parent_node || null;
+            var r = isroot || false;
+            var node = new jsMind.Node(o.nodeid,o.nodeindex,o.topic,o.summary,r,p);
+            return node;
+        },
+        _parse_extract_root:function(node_array){
+            var i = node_array.length;
+            while(i--){
+                if('isroot' in node_array[i] && node_array[i].isroot){
+                    var root_json = node_array[i];
+                    node_array.splice(i,1);
+                    var root_node = this._parse_node(root_json,null,true);
+                    this.nodes.push(root_node);
+                    return root_node;
+                }
+            }
+            return null;
+        },
+        _parse_extract_subnode:function(node_array,parent_node){
+            var pid = parent_node.Id;
+            var i = node_array.length;
+            var node_json = null;
+            var node = null;
+            var extract_count = 0;
+            while(i--){
+                node_json = node_array[i];
+                if(node_json.parentid == pid){
+                    node = this._parse_node(node_json,parent_node);
+                    parent_node.Children.push(node);
+                    this.nodes.push(node);
+                    node_array.splice(i,1);
+                    extract_count ++;
+                    var sub_extract_count = this._parse_extract_subnode(node_array,node);
+                    if(sub_extract_count > 0){
+                        // reset loop index after extract subordinate node
+                        i = node_array.length;
+                        extract_count += sub_extract_count;
+                    }
+                }
+            }
+            return extract_count;
+        },
         getRootNode:function(){},
         getSubNodes:function(nodeId){},
         getNode:function(nodeId){},
         addNode:function(node, parentid){},
-        removeNodes:function(nodeIdArray){}
+        removeNodes:function(nodeIdArray){},
+        getJsonArray:function(){
+        }
     };
 
-    jsMind._layout.prototype={
+    // layout provider
+    jsMind.layout_provider = function(jm, options){this.jsMind = jm; this.opts = options;};
+    jsMind.layout_provider.prototype={
         layout:function(fnCallback){
             _console.debug('layout.layout');
             if(typeof(fnCallback) === 'function'){
@@ -182,16 +280,20 @@
         }
     };
 
-    jsMind._viewer.prototype={
+    // view provider
+    jsMind.view_provider= function(jm, options){this.jsMind = jm; this.opts = options;};
+    jsMind.view_provider.prototype={
         show:function(fnCallback){
-            _console.debug('viewer.show');
+            _console.debug('view.show');
             if(typeof(fnCallback) === 'function'){
                 fnCallback();
             }
         }
     };
 
-    jsMind._theme.prototype={
+    // theme provider
+    jsMind.theme_provider= function(jm, options){this.jsMind = jm; this.opts = options;};
+    jsMind.theme_provider.prototype={
 
     };
 
