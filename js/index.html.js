2c7b2dc69db32f968795758a998dd6a6f6a3598a
hizzgdev <hizzgdev@163.com>
2014-03-03 20:53:24 +0800

    init website branch

---
+++ js/index.html.js
@@ -0,0 +1,9 @@
+(function($w){
+    var jsMind = $w.jsMind;
+
+    function page_load(){
+        var jm = jsMind.show({view:{container:'jsmind_container'}});
+    }
+
+    page_load();
+})(window);
