e02acca7f859714235e7b40e01e3da3365ee14d1
hizzgdev <hizzgdev@163.com>
2015-04-12 00:49:20 +0800

    replace uuid generator

--- js/jsmind.js
+++ js/jsmind.js
@@ -994,13 +994,7 @@
 
         uuid:{
             newid:function(){
-                var d = new Date().getTime();
-                var _uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    var r = (d + Math.random()*16)%16 | 0;
-                    d = Math.floor(d/16);
-                    return (c=='x' ? r : (r&0x7|0x8)).toString(16);
-                });
-                return _uuid;
+                return (new Date().getTime().toString(16)+Math.random().toString(16).substr(2)).substr(2,16);
             }
         }
     };
