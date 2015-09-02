2f3760952a58f35cd9622dee86140ee742b60a5d
zhangzhigang <zhangzhigang@tuotiansudai.com>
2015-09-03 00:00:53 +0800

    not complete

--- js/jsmind.screenshot.js
+++ js/jsmind.screenshot.js
@@ -13,22 +13,28 @@
     var jsMind = $w[__name__];
     if(!jsMind){return;}
     if(typeof jsMind.screenshot != 'undefined'){return;}
-    /*
-CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-  if (w < 2 * r) r = w / 2;
-  if (h < 2 * r) r = h / 2;
-  this.beginPath();
-  this.moveTo(x+r, y);
-  this.arcTo(x+w, y,   x+w, y+h, r);
-  this.arcTo(x+w, y+h, x,   y+h, r);
-  this.arcTo(x,   y+h, x,   y,   r);
-  this.arcTo(x,   y,   x+w, y,   r);
-  this.closePath();
-  return this;
-}
-*/
 
     var jcanvas = jsMind.util.canvas;
+    jcanvas.roundRect = function (ctx,x,y,w,h,r) {
+        if (w < 2 * r) r = w / 2;
+        if (h < 2 * r) r = h / 2;
+        ctx.beginPath();
+        ctx.moveTo(x+r, y);
+        ctx.arcTo(x+w, y,   x+w, y+h, r);
+        ctx.arcTo(x+w, y+h, x,   y+h, r);
+        ctx.arcTo(x,   y+h, x,   y,   r);
+        ctx.arcTo(x,   y,   x+w, y,   r);
+        ctx.closePath();
+    };
+
+    jcanvas.fillText = function (ctx,text,x,y,w,h){
+        var center_x = x+w/2;
+        var center_y = y+h/2;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(text,center_x,center_y);
+    };
+
 
     jsMind.screenshot = function(jm){
         this.jm = jm;
@@ -38,20 +44,56 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
         shoot:function(){
             var e_canvas = this._create_canvas();
             this._draw(e_canvas);
-            this._drop_canvas(_canvas);
+            this._drop_canvas(e_canvas);
         },
 
         _create_canvas:function(){
             var c = $d.createElement('canvas');
+            c.setAttribute('a','a');
             var jm_size = this.jm.view.size;
             c.width=jm_size.w;
             c.height=jm_size.h;
-            //this.jm.view.e_panel.appendChild(c);
+            this.jm.view.e_panel.appendChild(c);
             return c;
         },
 
         _draw:function(c){
             var ctx = c.getContext('2d');
+            ctx.textAlign = 'left';
+            ctx.textBaseline = 'top';
+            this._draw_nodes(ctx);
+        },
+
+        _draw_nodes:function(ctx){
+            var nodes = this.jm.mind.nodes;
+            var node;
+            for(var nodeid in nodes){
+                node = nodes[nodeid];
+                this._draw_node(ctx,node);
+            }
+        },
+
+        _draw_node:function(ctx,node){
+            var node_element = node._data.view.element;
+            var node_style = getComputedStyle(node_element);
+            var bgcolor = node_style.getPropertyValue('background-color');
+            var style_round_radius = node_style.getPropertyValue('border-radius');
+            var round_radius = parseInt(style_round_radius);
+            var color = node_style.getPropertyValue('color');
+            var font = node_style.getPropertyValue('font');
+            var view_data = node._data.view;
+
+            console.log(font);
+
+            ctx.font=font;
+            ctx.fillStyle = bgcolor;
+            jcanvas.roundRect(ctx, view_data.abs_x,view_data.abs_y,
+                view_data.width+2, view_data.height+2, round_radius);
+            ctx.fill();
+            ctx.fillStyle = color;
+            jcanvas.fillText(ctx, node.topic,
+                view_data.abs_x, view_data.abs_y,
+                view_data.width, view_data.height);
         },
 
         _drop_canvas:function(c){
