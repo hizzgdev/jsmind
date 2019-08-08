3fff1fd0bb020016caa8124920910ca6809764f4
hizzgdev <hizzgdev@163.com>
2019-08-09 00:00:06 +0800

    fix bug #146

--- js/jsmind.js
+++ js/jsmind.js
@@ -1159,7 +1159,9 @@
             var element = e.target || event.srcElement;
             var nodeid = this.view.get_binded_nodeid(element);
             if (!!nodeid) {
-                this.select_node(nodeid);
+                if (element.tagName.toLowerCase() == 'jmnode') {
+                    this.select_node(nodeid);
+                }
             } else {
                 this.select_clear();
             }
