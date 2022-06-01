b71b23afead0095550fe12564c46a8924b444a04
hizzgdev <hizzgdev@163.com>
2022-06-02 01:48:05 +0800

    add test for set root

--- tests/jsmind.mind.test.js
+++ tests/jsmind.mind.test.js
@@ -11,3 +11,18 @@ test('create mind', () => {
         nodes: {}
     })
 });
+
+test('set root', () => {
+    const mind = new jm.mind();
+    mind.set_root('1', 'root', { 'addition': 'test' });
+    const root_node = new jm.node('1', 0, 'root', { 'addition': 'test' }, true);
+    const expect_mind = {
+        name: null,
+        author: null,
+        version: null,
+        root: root_node,
+        selected: null,
+        nodes: { '1': root_node }
+    }
+    expect(mind).toEqual(expect_mind);
+})
