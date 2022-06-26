56466e76b886ebf947a2d96f099103a93fa8e433
hizzgdev <hizzgdev@163.com>
2022-06-27 01:07:03 +0800

    add unit test for graph module

--- tests/unit/jsmind.format.test.js
+++ tests/unit/jsmind.format.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import { format } from '../../src/jsmind.format.js';
 import { Direction } from '../../src/jsmind.common.js';
 
