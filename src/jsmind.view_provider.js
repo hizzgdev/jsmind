58aae698c33bdb0a449bfed66f05f56e47691072
hizzgdev <hizzgdev@163.com>
2022-09-27 20:31:10 +0800

    fix bug to scroll mind when zoom in

--- src/jsmind.view_provider.js
+++ src/jsmind.view_provider.js
@@ -331,7 +331,7 @@ export class ViewProvider {
         }
         this.actualZoom = zoom;
         for (var i = 0; i < this.e_panel.children.length; i++) {
-            this.e_panel.children[i].style.transform = 'scale(' + zoom + ')';
+            this.e_panel.children[i].style.zoom = zoom;
         }
         this.show(true);
         return true;
