7bb4e6573b9470010f373b8a2fc1b44e1071f70c
Zhigang Zhang <hizzgdev@163.com>
2023-05-26 01:42:27 +0800

    release 0.6.0 (#465)

--- src/jsmind.common.js
+++ src/jsmind.common.js
@@ -6,7 +6,7 @@
  *   https://github.com/hizzgdev/jsmind/
  */
 
-export const __version__ = '0.5.7';
+export const __version__ = '0.6.0';
 export const __author__ = 'hizzgdev@163.com';
 
 if (typeof String.prototype.startsWith != 'function') {
