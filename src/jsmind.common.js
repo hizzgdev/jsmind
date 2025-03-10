0694ed3ed66a4f6fb5e196515ed550f1863d9914
hizzgdev <hizzgdev@163.com>
2025-03-10 17:11:31 +0800

    update version to 0.8.7

--- src/jsmind.common.js
+++ src/jsmind.common.js
@@ -6,7 +6,7 @@
  *   https://github.com/hizzgdev/jsmind/
  */
 
-export const __version__ = '0.8.6';
+export const __version__ = '0.8.7';
 export const __author__ = 'hizzgdev@163.com';
 
 if (typeof String.prototype.startsWith != 'function') {
